1f711a6edd8a7856ad15184580bd1524e6a0c276
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-05-10 15:11:20 -0500

    Added feature to return random tarot card on /hello endpoint.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,22 @@
 import io.javalin.Javalin;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
+        List<String> tarotDeck = Arrays.asList(
+                "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant",
+                "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man",
+                "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"
+        );
+
         Javalin app = Javalin.create().start(7000);
-        app.get("/hello", ctx -> ctx.result("Hello, World!"));
+        app.get("/hello", ctx -> ctx.result(drawRandomCard(tarotDeck)));
+    }
+
+    public static String drawRandomCard(List<String> deck) {
+        Random rand = new Random();
+        return deck.get(rand.nextInt(deck.size()));
     }
 }
